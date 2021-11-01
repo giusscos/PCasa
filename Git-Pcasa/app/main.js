f0504fb27e2054fb05c5fb9ec1b75f3e59a70156
Giuseppe Pio Cosenza <63466096+Giusscos08@users.noreply.github.com>
2021-11-01 20:27:23 +0100

    Delete Git-Pcasa directory

--- Git-Pcasa/app/main.js
+++
@@ -1,5 +0,0 @@
-const selectElement = (element) => document.querySelector(element);
-
-selectElement('.show-menu').addEventListener('click', () => {
-    selectElement('header').classList.toggle('active');
-})
