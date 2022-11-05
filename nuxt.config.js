f67279987fb7c0f6d9b693c44de59349d86548b5
giusscos <giu.cosenza08@gmail.com>
2022-11-05 18:15:14 +0100

    Deploy test

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,8 @@
 export default {
+  target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Pcasa',
+    title: 'Detersivi Ecologici e Prodotti Tipici Calabresi - Pcasa',
     htmlAttrs: {
       lang: 'it'
     },
