150726cfe262d505d3fb8bb6ef3defd2eea1502c
giusscos <giu.cosenza08@gmail.com>
2025-01-07 22:20:00 +0100

    Refactor homepage section for improved layout and content presentation

--- pages/index.vue
+++ pages/index.vue
@@ -44,32 +44,32 @@
       </div>
     </div>
   </section>
-  <Categories />
-  <section class="section py-20">
-    <div class="container mx-auto px-4">
-      <h2 class="pb-4 title-sm leading-none">
-        Siamo un azienda che pensa in <span
-          class="inline-block bg-pcasa-accent-alt px-2 md:px-4 pb-2 md:pb-4 rounded-full leading-none">verde</span>
-      </h2>
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-8">
-        <div class="w-full rounded-lg overflow-hidden">
-          <img width="400" class="img-cover" src="/img/team-pcasa.webp" alt="Il team pcasa" loading="lazy">
-        </div>
-        <div class="w-full flex flex-col gap-y-4">
-          <p class="paragraph-standard">
-            Proponiamo soluzioni verdi e smart per rispettare
-            l'ambiente che ci circonda. Contiamo nell'aiuto di
-            ogni singolo individuo per poter cambiare in meglio il modo in cui
-            interagiamo con l'ambiente.
-            <br><br>
-            Abbiamo obiettivi ambiziosi ma... qualcuno deve pur farlo.
-            E tu, vorresti unirti a noi?🌱
-          </p>
-          <NuxtLink to="/contatti" title="Scopri come entrare nel nostro team"
-            class="btn-standard border-pcasa-accent-alt hover:bg-pcasa-accent-alt text-pcasa-accent-alt hover:text-pcasa-text">
-            Scopri come
-          </NuxtLink>
-        </div>
+
+  <!-- <Categories /> -->
+
+  <section class="section py-20 sm:container sm:mx-auto">
+
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-8">
+      <div class="rounded-lg overflow-hidden">
+        <img width="400" src="/img/team-pcasa.webp" alt="Il team pcasa"
+          class="w-full h-full object-cover object-top aspect-square" loading="lazy">
+      </div>
+      <div class=" flex flex-col gap-y-4">
+        <h2 class="text-4xl font-bold">
+          Il nostro impegno verso il pianeta
+        </h2>
+        <p class="text-xl py-4">
+          Proponiamo soluzioni verdi e smart per rispettare
+          l'ambiente che ci circonda. Contiamo nell'aiuto di
+          ogni singolo individuo per poter cambiare in meglio il modo in cui
+          interagiamo con l'ambiente.
+          Abbiamo obiettivi ambiziosi ma... qualcuno deve pur farlo.
+          E tu, vorresti unirti a noi?🌱
+        </p>
+        <NuxtLink to="/contatti" title="Scopri come entrare nel nostro team"
+          class="btn-standard border-pcasa-accent-alt hover:bg-pcasa-accent-alt text-pcasa-accent-alt hover:text-pcasa-text">
+          Scopri come
+        </NuxtLink>
       </div>
     </div>
   </section>
